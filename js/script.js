497db7e62c9795b6ce279623373228483c6fd2df
AnderRester <AnderRester@icloud.com>
2022-03-20 15:26:25 +0200

    fixes

--- js/script.js
+++ js/script.js
@@ -34,23 +34,47 @@ openFile.onchange = () => {
 }
 
 const encryptOperation = () => {
-    if(key.value == "") {
-    alert("You have't entered a key");
+    if (key.value == "") {
+        alert("You have not entered a key");
     } else {
         let encryptField_sub = encryptField.value.split("", encryptField.value.length);
         let key_sub = key.value.split("", key.value.length);
         let keyPosition = [];
-            for(let j = 0; j < en_alphabet.length; j++) {
-                if(key_sub[j] == en_alphabet[j]) {
+        //              Key position
+        for (let j = 0; j < en_alphabet.length; j++) {
+            for (let k = 0; k < key_sub.length; k++) {
+                if (key_sub[k] == en_alphabet[j]) {
                     keyPosition.push(j);
                 }
+            }
         }
-        // for(let i = 0; i < encryptField_sub.length; i++) {
-            
-        // }
-        console.log(keyPosition);
-        console.log(encryptField_sub);
-        console.log(key.value.length);
+        //              Encryption prepare
+        
+        encryptField_sub = encryptField_sub.map(space => space == " " ? space = "*" : space);
+        let encryptFieldArray = [];
+        while(encryptField_sub.length%key_sub.length != 0) {
+            encryptField_sub.push("*")
+        };
+        encryptFieldArray.push(0);
+        for(let i = 0; i < encryptField_sub.length; i += key_sub.length) {
+            encryptFieldArray.push(encryptField_sub.slice(i, i + key_sub.length));
+        }
+        console.log(encryptFieldArray);
+        //              Encryption process
+        let finalEncryptedArray = "";
+        let keyIdAccess = 0;
+        for(let i = 1; i < encryptFieldArray.length; i++) {
+            if(i%key_sub.length == 0){
+                finalEncryptedArray += "_";
+                if(keyIdAccess < key_sub.length){
+                    keyIdAccess++;
+                }
+            } else {
+                finalEncryptedArray += encryptFieldArray[i][keyIdAccess];
+            }
+        }
+        console.log(finalEncryptedArray);
+        console.log(finalEncryptedArray);
     }
 }
 
